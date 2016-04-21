f8ef9e6943c826e3864fa90fb0691b7fcef40b76
Piotr Czajkowski <czajkowski.piotr@gmail.com>
2016-04-21 17:38:40 +0200

    Added /dist to test pages.

---
+++ dist/main.bundle.js
@@ -0,0 +1,192 @@
+webpackJsonp([0],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var browser_1 = __webpack_require__(1);
+	var core_1 = __webpack_require__(24);
+	var router_1 = __webpack_require__(229);
+	var button_1 = __webpack_require__(258);
+	var card_1 = __webpack_require__(259);
+	var checkbox_1 = __webpack_require__(260);
+	var sidenav_1 = __webpack_require__(261);
+	var input_1 = __webpack_require__(263);
+	var list_1 = __webpack_require__(265);
+	var radio_1 = __webpack_require__(266);
+	var progress_circle_1 = __webpack_require__(268);
+	var toolbar_1 = __webpack_require__(269);
+	var app_component_1 = __webpack_require__(270);
+	browser_1.bootstrap(app_component_1.AppComponent, [
+	    router_1.ROUTER_PROVIDERS,
+	    core_1.provide(core_1.PLATFORM_DIRECTIVES, {
+	        useValue: [
+	            card_1.MD_CARD_DIRECTIVES,
+	            sidenav_1.MD_SIDENAV_DIRECTIVES,
+	            input_1.MD_INPUT_DIRECTIVES,
+	            list_1.MD_LIST_DIRECTIVES,
+	            [
+	                button_1.MdButton,
+	                button_1.MdAnchor,
+	                checkbox_1.MdCheckbox,
+	                radio_1.MdRadioButton,
+	                progress_circle_1.MdSpinner,
+	                progress_circle_1.MdProgressCircle,
+	                toolbar_1.MdToolbar
+	            ]
+	        ],
+	        multi: true
+	    }),
+	    core_1.provide(router_1.LocationStrategy, { useClass: router_1.HashLocationStrategy })
+	]);
+
+
+/***/ },
+
+/***/ 270:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(24);
+	var router_1 = __webpack_require__(229);
+	var public_panel_component_1 = __webpack_require__(271);
+	var admin_panel_component_1 = __webpack_require__(274);
+	var AppComponent = (function () {
+	    function AppComponent() {
+	    }
+	    AppComponent = __decorate([
+	        core_1.Component({
+	            selector: 'domofon-app',
+	            template: __webpack_require__(277),
+	            styles: [__webpack_require__(278)],
+	            directives: [router_1.ROUTER_DIRECTIVES]
+	        }),
+	        router_1.RouteConfig([
+	            { path: '/public', name: 'PublicPanel', component: public_panel_component_1.PublicPanelComponent, useAsDefault: true },
+	            { path: '/admin', name: 'AdminPanel', component: admin_panel_component_1.AdminPanelComponent },
+	        ]), 
+	        __metadata('design:paramtypes', [])
+	    ], AppComponent);
+	    return AppComponent;
+	}());
+	exports.AppComponent = AppComponent;
+
+
+/***/ },
+
+/***/ 271:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(24);
+	var PublicPanelComponent = (function () {
+	    function PublicPanelComponent() {
+	    }
+	    PublicPanelComponent = __decorate([
+	        core_1.Component({
+	            selector: 'domofon-app',
+	            template: __webpack_require__(272),
+	            styles: [__webpack_require__(273)]
+	        }), 
+	        __metadata('design:paramtypes', [])
+	    ], PublicPanelComponent);
+	    return PublicPanelComponent;
+	}());
+	exports.PublicPanelComponent = PublicPanelComponent;
+
+
+/***/ },
+
+/***/ 272:
+/***/ function(module, exports) {
+
+	module.exports = "<h2>Public panel</h2>\r\n\r\n<md-card>\r\n  <md-card-content>\r\n    <md-card-subtitle>Subtitle first</md-card-subtitle>\r\n    <form>\r\n      <md-input\r\n        placeholder=\"Submit Local State to App State\"\r\n        autofocus>\r\n      </md-input>\r\n\r\n      <button md-raised-button color=\"primary\">Submit Value</button>\r\n    </form>\r\n\r\n  </md-card-content>\r\n</md-card>\r\n"
+
+/***/ },
+
+/***/ 273:
+/***/ function(module, exports) {
+
+	module.exports = ""
+
+/***/ },
+
+/***/ 274:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(24);
+	var AdminPanelComponent = (function () {
+	    function AdminPanelComponent() {
+	    }
+	    AdminPanelComponent = __decorate([
+	        core_1.Component({
+	            selector: 'domofon-app',
+	            template: __webpack_require__(275),
+	            styles: [__webpack_require__(276)]
+	        }), 
+	        __metadata('design:paramtypes', [])
+	    ], AdminPanelComponent);
+	    return AdminPanelComponent;
+	}());
+	exports.AdminPanelComponent = AdminPanelComponent;
+
+
+/***/ },
+
+/***/ 275:
+/***/ function(module, exports) {
+
+	module.exports = "<h2>Admin panel</h2>"
+
+/***/ },
+
+/***/ 276:
+/***/ function(module, exports) {
+
+	module.exports = ""
+
+/***/ },
+
+/***/ 277:
+/***/ function(module, exports) {
+
+	module.exports = "<h1>Domofon</h1>\r\n\r\n<main>\r\n    <router-outlet></router-outlet>\r\n</main>"
+
+/***/ },
+
+/***/ 278:
+/***/ function(module, exports) {
+
+	module.exports = "h1 { color: #f00; }"
+
+/***/ }
+
+});
